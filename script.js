38195544d3b1fec63c019b0c9283e8756efc19a2
Ryncet <30adityaa@students.harker.org>
2025-03-05 14:30:07 -0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function affirm(){
-    let nameInput =  document.getElementById('name');
+function fName(){
+    let nameInput =  document.getElementById('fName');
     let name = nameInput.value;
-    let affirmation = name + ", be my valentine";
+    let affirmation = name;
     let affirmationDisplay = document.getElementById('affirmation-display');
     affirmationDisplay.innerHTML= affirmation;
 }
+function lName(){
+    let nameInput =  document.getElementById('lName');
+    let name = nameInput.value;
+    let affirmation = name;
+    let affirmationDisplay = document.getElementById('affirmation-display1');
+    affirmationDisplay.innerHTML= affirmation;
+}
+function birth(){
+    let nameInput =  document.getElementById('Birth-Date');
+    let name = nameInput.value;
+    let affirmation = name;
+    let affirmationDisplay = document.getElementById('affirmation-display2');
+    affirmationDisplay.innerHTML= affirmation;
+}
+function address(){
+    let nameInput =  document.getElementById('address');
+    let name = nameInput.value;
+    let affirmation = name;
+    let affirmationDisplay = document.getElementById('affirmation-display4');
+    affirmationDisplay.innerHTML= affirmation;
+}
+
